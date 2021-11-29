ff747115b545cad86d0bd4cbf3a6a8f71dbbeec1
p22jdiao <p22jdiao@fairfieldprep.org>
2021-11-29 10:59:53 -0500

    1-1, 1-2

---
+++ lab_1-1.py
@@ -0,0 +1,11 @@
+# Author: JD 11/29/2021
+
+def greeting():
+    '''
+    This function returns "Hello World!" by using a print() statement'''
+    print("Hello World!")
+
+
+help(greeting)
+
+greeting()
---
+++ lab_1-2.py
@@ -0,0 +1,13 @@
+# Author: JD 11/29/2021
+
+def find_sum(num_1, num_2):
+    num_sum = num_1 + num_2
+    return(num_sum)
+
+number_1 = 111
+
+number_2 = 222
+
+my_sum = find_sum(number_1, number_2)
+
+print(my_sum)
